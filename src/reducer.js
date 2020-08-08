4a53f9776da642cc3de1c74967e21b5034297aac
Abhin Pai <abhinpai96@gmail.com>
2020-08-09 02:37:36 +0530

    Updated reducer

--- src/reducer.js
+++ src/reducer.js
@@ -3,9 +3,7 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
-  // Set token to null once everything is done this is just of development purpose
   token: null
-  // token: `BQAIJ1lj2rS0wTtM8Zhro0kJsavDo4HbIMwFFPKjQkEbAv2eAxxWQuUEsfOH4VwE9esQ8lBhatNpSyc79xmGhv8GSSzAblGL0HRVpj5PABZLlQYXZ1oUeO64uIqCYZHkN1ie83uH_qD47HrMX0XMcIO3Rzpy5V4oklxcqH6xlgPiWCVoFgLU`,
 };
 
 const reducer = (state, action) => {
