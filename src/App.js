6ead4a3c7f28cbb1f7888c424c405ea001dd54f4
Abhin Pai <abhinpai96@gmail.com>
2020-08-08 22:50:40 +0530

    Initial code of spotify clone 🚀

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokeFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokeFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+  return <div className='app'>{token ? <h2>I logged</h2> : <Login />}</div>;
 }
 
 export default App;
